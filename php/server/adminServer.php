f9b8b1e0928cf8a53d7abd6cb5eb594f901c6a8d
JAYJAY <2422301985@qq.com>
2023-12-15 14:27:52 +0800

    小补丁

--- php/server/adminServer.php
+++ php/server/adminServer.php
@@ -6,7 +6,7 @@ require_once '../dataBase/mysqli_connect.php';
 header("Content-Type: application/json;charset=utf-8");
 session_start();
 if (!isset($_SESSION['is_logged_in']) || $_SESSION['is_logged_in'] !== true) {
-    header('Location: login.php');
+    header('Location: ../web/login.php');
     exit;
 }
 $changePower=function ($dbc){
