b2adc30e1c507db676222c271eb3bde7d6b72b6d
JAYJAY <2422301985@qq.com>
2024-01-08 21:59:39 +0800

    补丁

--- php/web/homeTemplate.php
+++ php/web/homeTemplate.php
@@ -16,7 +16,7 @@ $user=$_SESSION['user'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>首页</title>
     <script src="../../static/js/jquery-3.7.1.min.js"></script>
     <link rel="stylesheet" href="../../static/css/meyer.css">
     <link rel="stylesheet" href="../../static/css/homeTemplate.css">
